8baafed7aa2182c37f7a33454158ead8382c6c86
Kirsten <1010sheehy@gmail.com>
2021-01-05 13:03:36 -0800

    Added Lab1.rmd and notes
    Input Lab1.rmd and week 1 notes

---
+++ PLS205_Winter2021/class_notes/Intro_notes.R
@@ -0,0 +1,52 @@
+# Week 1 Notes ----
+
+# Runcie Lab - corn, mimulus, arabidopsis genetics and evolution
+# dipping into AI analyses
+
+# MAIN GOALS ----
+
+# 1. How do you design a good experiment?
+# 2. Once you've run a good experiment, how do you analyze and communicate
+#    your results?
+
+# Specific objectives ----
+
+# be able to choose and appropriate exp. design
+# treatments
+# replication
+# randomization
+# blocking
+
+# ID and eval exp. designs of other researchers
+# socpe
+# goals
+# efficiency
+# Problems? bias, confounding, pseudo-replication
+
+# Analyze data and communicate results from a range of exp. with R
+# parameter estimation
+# hypothesis testing
+# produce reports using Rmd
+
+
+# Homework
+
+# Due before class on Thurs
+# Rubric released on Fri
+# Self grade due following Thurs
+
+# Review ----
+
+# t-test
+# ANOVA
+# Distributions (normal, Student's t)
+# more...
+
+# Textbook reccomendation: Sata meth in bio: Design and Analysis of Exp and Reg, Welham, Gezan, Clark, Mead, Press
+
+
+
+
+
+
+
